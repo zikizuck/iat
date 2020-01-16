02537bab4ca2636468f0157e7e48fe7b8d35f36d
Ziki Zuck <ziki.zuck@gmail.com>
2020-01-16 13:02:07 +0200

    Updates

---
+++ precache-manifest.26a45d8e8604cc9de2400981974f9316.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806ac6b56ecba4a2748fdcaa395d51b9",
+    "url": "/iat/index.html"
+  },
+  {
+    "revision": "b2f88fe81768a2325a6b",
+    "url": "/iat/static/css/2.a1f70397.chunk.css"
+  },
+  {
+    "revision": "6dfe7c1f9a56d22df1bf",
+    "url": "/iat/static/css/main.da1017cc.chunk.css"
+  },
+  {
+    "revision": "b2f88fe81768a2325a6b",
+    "url": "/iat/static/js/2.d7b92dab.chunk.js"
+  },
+  {
+    "revision": "a34ff0125cab7c32bc50ba794786f65d",
+    "url": "/iat/static/js/2.d7b92dab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6dfe7c1f9a56d22df1bf",
+    "url": "/iat/static/js/main.8dff88cd.chunk.js"
+  },
+  {
+    "revision": "91c40697809fc1efaf18",
+    "url": "/iat/static/js/runtime-main.e9043fc6.js"
+  }
+]);
