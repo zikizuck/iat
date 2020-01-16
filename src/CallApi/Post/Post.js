19e097f1c5e23d92abe73e054eae251bb00b7377
Ziki Zuck <ziki.zuck@gmail.com>
2020-01-16 22:59:30 +0200

    jeff list

--- src/CallApi/Post/Post.js
+++ src/CallApi/Post/Post.js
@@ -23,16 +23,17 @@ class Post extends Component {
                         <div className="card-body">
                             <h5 className="card-title">{this.props.item}</h5>
                             <p className="card-text">W.A.I # :{this.props.WAI}</p>
-                            <p className="card-text">YouTech # :{this.props.YouTech}</p>
+                            <p className="card-text">YouTech # :{this.props.YT}</p>
                             <p className="card-text">Lester # :{this.props.Lester}</p>
+                            <p className="card-text">J & N # :{this.props.JN}</p>
                             <p className="card-text">Class :{this.props.class}</p>
                             <p className="card-text">{this.props.vpartno} </p>
-                            <p className="card-text" >{this.props.description}</p>
-                            <p className= {cls}  >In Stock: {this.props.onhand_quantity}</p>
+                            <p className="card-text" >{this.props.descrip}</p>
+                            <p className= {cls}  >In Stock: {this.props.onhand}</p>
                             {/*<p className={cls2} >{this.props.stock5}  : במלאי מחסן 5 </p>*/}
                             {/*<p className="card-text">{this.props.Amp}Amp </p>*/}
                             {/*<p className="card-text">{this.props.R} {this.props.N}</p>*/}
-                            <p className="card-text">IAT Cost Price :{this.props.unit_cost}  </p>
+                            <p className="card-text">IAT Price :{this.props.price}  </p>
                             {/*<p className="card-text" dir="rtl">*/}
                             {/*    <FontAwesomeIcon icon={faSearchLocation} style={{color:"blue"}}/>*/}
                             {/*    {this.props.FirstFloor} כמות- {this.props.stock1} <br/> {this.props.secFloor}   כמות-{this.props.stock2}</p>*/}
